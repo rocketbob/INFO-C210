9fde879f3f7e5034f22b4060f482735a58ff1173
PARRISH CAMERON <SESA91827@schneider-electric.com>
2018-12-08 16:52:44 -0500

    Added A New Color to the top of each month as theme

--- CalendarPanel.java
+++ CalendarPanel.java
@@ -3,10 +3,17 @@ import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CalendarPanel extends JPanel {
+	//ImageIcon icon = new ImageIcon(imgURL); 
+	
+	
+	private Color[] monthHeader = {Color.red,Color.blue,Color.green,Color.BLACK,Color.lightGray
+			,Color.cyan,Color.magenta,Color.DARK_GRAY,Color.WHITE,Color.orange,Color.pink,Color.yellow};
+	
 	private String[] daysOfWeek = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 	// private String[] monthsOfYear = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 	int tailSet;
@@ -14,16 +21,17 @@ public class CalendarPanel extends JPanel {
 	private static final int LIGHTGRAY = 0;
 	private static final int YELLOW = 2;
 	private static final int RED = 3;
-	private static int offsetBlanks = 0;
 
-	// problem with September and December when offset is 6 it dosen't work?
+	
 	public CalendarPanel (int month, int border, GregorianCalendar gCalendar) {
 		JPanel tileContainer = new JPanel();
 		JPanel headingPanel = new JPanel();
 		JLabel monthLabel = new JLabel(SidePanel.monthList[month]);
 		monthLabel.setForeground(Color.WHITE);
 		monthLabel.setHorizontalAlignment(JLabel.CENTER);
-		headingPanel.setBackground(Color.GRAY);
+		
+		
+		headingPanel.setBackground(monthHeader[month]);
 		headingPanel.add(monthLabel);
 		this.tailSet = 42 - (gCalendar.monthNumDays[month] + gCalendar.monthOffsets[month]); // figue up number of trailing tiles needed
 		tileContainer.setLayout(new GridLayout(7,7));
